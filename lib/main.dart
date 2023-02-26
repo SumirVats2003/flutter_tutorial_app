fabee268d5c2b555aca3854dfb56471fe0273f8a
Sumir Vats <sumirvats@gmail.com>
2023-02-26 19:50:47 +0530

    Material Drawer and Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MaterialApp(
     home: const HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.purple,
+      primarySwatch: Colors.blue,
     ),
   ));
 }
@@ -17,36 +17,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Awesome App")),
-      body: Container(
-        height: 500,
-        color: Colors.indigo,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.green,
-            ),
-          ],
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.indigo,
         ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.edit),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: const <Widget> [
+            UserAccountsDrawerHeader(
+              accountName: Text("Sumir Vats"), accountEmail: Text("sumirvats@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+                ),
+              )
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Sumir Vats"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("sumirvats@gmail.com"),
+              trailing: Icon(Icons.edit),
+            ),
+          ]
+        )
       ),
     );
   }
